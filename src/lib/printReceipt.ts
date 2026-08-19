8cf09bc5996b20bc9b533c8b5e0eda66ceccbe01
JOKO TODAY <jokotoday@gmail.com>
2026-08-19 10:50:15 +0700

    Keep receipt helper compatible with current target

--- src/lib/printReceipt.ts
+++ src/lib/printReceipt.ts
@@ -29,11 +29,11 @@ type PrintReceiptOptions = {
 };
 
 const escapeHtml = (value: unknown) => String(value ?? '')
-  .replaceAll('&', '&amp;')
-  .replaceAll('<', '&lt;')
-  .replaceAll('>', '&gt;')
-  .replaceAll('"', '&quot;')
-  .replaceAll("'", '&#039;');
+  .replace(/&/g, '&amp;')
+  .replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;')
+  .replace(/"/g, '&quot;')
+  .replace(/'/g, '&#039;');
 
 const money = (value: unknown) => {
   const amount = Number(value);
