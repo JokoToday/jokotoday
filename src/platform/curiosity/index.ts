a2ddebf86395827151564c2a0a131208dd585fee
JOKO TODAY <jokotoday@gmail.com>
2026-09-14 14:32:17 +0700

    feat: export Curiosity embed surface

--- src/platform/curiosity/index.ts
+++ src/platform/curiosity/index.ts
@@ -1,4 +1,5 @@
 export * from './contracts';
 export * from './validation';
 export * from './notebookAdapter';
+export * from './embed/CuriosityEmbed';
 export { jokoTodayCuriosityFixture } from './fixtures/jokoTodayCuriosityFixture';
